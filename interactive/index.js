fece144d822b6178e67b7fe7e286b0eb17ec92f3
carreb <32069843+carreb@users.noreply.github.com>
2022-05-04 13:15:49 -0400

    dummy functions for displaying text

---
+++ interactive/index.js
@@ -0,0 +1,10 @@
+const infoText = document.getElementById('prompt-display');
+const titleText = document.getElementById('text-display');
+
+function displayText(text) {
+    infoText.innerHTML = text;
+}
+
+function displayTitle(text) {
+    titleText.innerHTML = text;
+}
